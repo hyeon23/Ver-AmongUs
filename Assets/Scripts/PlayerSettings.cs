65dbf26e5e92aa6aa1e7973e31945e510689fca8
hyeon23 <urim2270@naver.com>
2024-04-13 20:53:44 +0900

    feat: Settings Develop #2
    -환경설정 버튼 연결 시, 패널 등장 로직 구현
    -환경설정 기능 개발

---
+++ Assets/Scripts/PlayerSettings.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse,
+    Count
+};
+
+public class PlayerSettings
+{
+    public static EControlType controlType;
+}
---
+++ Assets/Scripts/SettingUI.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingUI : MonoBehaviour
+{
+    [Header("UI Objects")]
+    [SerializeField]
+    private Button MouseControlButton;
+    [SerializeField]
+    private Button KeyboardMouseControlButton;
+    [SerializeField]
+    private TextMeshProUGUI MouseTMP;
+    [SerializeField]
+    private TextMeshProUGUI KeyboardMouseTMP;
+
+    [Header("Animator")]
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        switch (PlayerSettings.controlType)
+        {
+            case EControlType.Mouse:
+                MouseControlButton.image.color = Color.green;
+                MouseTMP.color = Color.green;
+                KeyboardMouseControlButton.image.color = Color.white;
+                KeyboardMouseTMP.color = Color.white;
+                break;
+            case EControlType.KeyboardMouse:
+                MouseControlButton.image.color = Color.white;
+                MouseTMP.color = Color.white;
+                KeyboardMouseControlButton.image.color = Color.green;
+                KeyboardMouseTMP.color = Color.green;
+                break;
+            case EControlType.Count:
+                break;
+        }
+    }
+
+    public void SetControlMode(int controlType)
+    {
+        PlayerSettings.controlType = (EControlType)controlType;
+
+        switch (PlayerSettings.controlType)
+        {
+            case EControlType.Mouse:
+                MouseControlButton.image.color = Color.green;
+                KeyboardMouseControlButton.image.color = Color.white;
+                break;
+            case EControlType.KeyboardMouse:
+                MouseControlButton.image.color = Color.white;
+                KeyboardMouseControlButton.image.color = Color.green;
+                break;
+            case EControlType.Count:
+                break;
+        }
+    }
+
+    public void Close()
+    {
+        StartCoroutine(CloseAfterDelay());
+    }
+
+    private IEnumerator CloseAfterDelay()
+    {
+        animator.SetTrigger("close");
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+        animator.ResetTrigger("close");
+    }
+}
