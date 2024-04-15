e76c058942235853ad2721a10bf784416aec2f09
hyeon23 <urim2270@naver.com>
2024-04-15 14:40:18 +0900

    feat: Lobby Drop Ship Import
    -에셋 제작 및 임포트
    -fix: ShaderGraph에 따른 적용 문제 해결(네모 박스)

--- Assets/Scripts/OnlineUI.cs
+++ Assets/Scripts/OnlineUI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
 public class OnlineUI : MonoBehaviour
 {
@@ -18,6 +19,19 @@ public class OnlineUI : MonoBehaviour
             PlayerSettings.nickname = nicknameInputField.text;
             createRoomUI.SetActive(true);
             gameObject.SetActive(false);
+        }
+        else
+        {
+            nicknameInputField.GetComponent<Animator>().SetTrigger("on");
+        }
+    }
+
+    public void OnClickEnterGameRoom()
+    {
+        if (nicknameInputField.text != "")
+        {
+            var manager = AmongUsRoomManager.singleton;
+            manager.StartClient();
         }
         else
         {
