c9866fb6c96c8c56420d3599a5f46466da5ec394
hyeon23 <urim2270@naver.com>
2024-04-17 00:08:39 +0900

    feat: y 좌표에 따른 SpriteOrdering 시스템 구현
    -y 좌표에 따라, 스프라이트의 SpriteOrdering을 조정하는 기능 개발
    -Update/Static으로 나눠, Update의 경우 지속적으로 y 좌표에 따른 SpriteOrdering 값 계산
    -fix: back & front의 값을 GameObject가 상회하여 SpriteOrdering의 계산이 잘못되는 현상 수정

---
+++ Assets/Scripts/SpriteSorter.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteSorter : MonoBehaviour
+{
+    ///높이에 따른 Sprite 정렬 기능
+    //-Back과 Front의 값을 여유를 줘서 설정
+    //-절대 값을 계산하므로, GameObject의 좌표가 Back or Front를 상회하는 경우 문제 발생
+    [SerializeField]
+    private Transform Back;
+    [SerializeField]
+    private Transform Front;
+    public int GetSortingOrder(GameObject go)
+    {
+        float objDist = Mathf.Abs(Back.position.y - go.transform.position.y);
+        float totalDist = Mathf.Abs(Back.position.y - Front.position.y);
+
+        return (int)(Mathf.Lerp(System.Int16.MinValue, System.Int16.MaxValue, objDist / totalDist));
+    }
+}
