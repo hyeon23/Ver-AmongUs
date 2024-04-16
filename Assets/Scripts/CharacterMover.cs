9e78a3e8fce98827af6df8d85511ee5bc0a66c04
hyeon23 <urim2270@naver.com>
2024-04-16 15:37:41 +0900

    feat: Mirror Multiplay Character Move Develop #1
    -멀티 플레이 환경에서 캐릭터 이동 개발
    -fix: 그 외 설정 창에서 close 버튼이 작동되지 않는 현상 수정

---
+++ Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Org.BouncyCastle.Bcpg;
+
+public class CharacterMover : NetworkBehaviour
+{
+    public bool isMovable;
+
+    [SyncVar]//네트워크 동기화
+    public float speed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isOwned)
+        {
+            Camera cam = Camera.main;
+            cam.transform.SetParent(transform);
+            cam.transform.localPosition = new Vector3(0f, 0f, -10f);
+            cam.orthographicSize = 2.5f;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        if(isOwned && isMovable)//권한 O & 이동 가능
+        {
+            if(PlayerSettings.controlType == EControlType.Mouse)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    //조작 방식에 따른 방향벡터
+                    Vector3 dir = (Input.mousePosition - new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0f)).normalized;
+
+                    //스프라이트 반전
+                    if (dir.x < 0f) transform.localScale = new Vector3(-0.75f, 0.75f, 1f);
+                    else if (dir.x > 0f) transform.localScale = new Vector3(0.75f, 0.75f, 1f);
+
+                    //프레임 별 이동
+                    transform.position += dir * speed * Time.deltaTime;
+                }
+            }
+            else if (PlayerSettings.controlType == EControlType.KeyboardMouse)
+            {
+                //조작 방식에 따른 방향벡터
+                Vector3 dir = Vector3.ClampMagnitude(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f), 1f);
+                
+                //스프라이트 반전
+                if (dir.x < 0f) transform.localScale = new Vector3(-0.75f, 0.75f, 1f);
+                else if (dir.x > 0f) transform.localScale = new Vector3(0.75f, 0.75f, 1f);
+                
+                //프레임 별 이동
+                transform.position += dir * speed * Time.deltaTime;
+            }
+        }
+    }
+}
--- Assets/Scripts/GameRoomSettingUI.cs
+++ Assets/Scripts/GameRoomSettingUI.cs
@@ -6,6 +6,8 @@ public class GameRoomSettingUI : SettingUI
 {
     public void ExitGameRoom()
     {
+        //Host Migration
+        //-호스트가 종료할 경우, 클라이언트가 Host의 권한을 위임받는 기능 개발 필요
         var manager = AmongUsRoomManager.singleton;
         if(manager.mode == Mirror.NetworkManagerMode.Host)
         {
