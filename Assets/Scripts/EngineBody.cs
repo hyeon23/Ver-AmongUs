ee5321a17a81794a51232cc9b98427140ad6f762
hyeon23 <77566434+hyeon23@users.noreply.github.com>
2024-04-26 22:22:41 +0900

    feat: The Skeld Map 개발 완료

--- Assets/Scripts/EngineBody.cs
+++ Assets/Scripts/EngineBody.cs
@@ -6,6 +6,12 @@ public class EngineBody : MonoBehaviour
 {
     [SerializeField]
     public List<GameObject> steams = new List<GameObject>();
+    [SerializeField]
+    private List<SpriteRenderer> sparks = new List<SpriteRenderer>();
+    [SerializeField]
+    private List<Sprite> sparkSprites = new List<Sprite>();
+
+    private int nowIdx = 0;
 
     private void Start()
     {
@@ -13,6 +19,7 @@ public class EngineBody : MonoBehaviour
         {
             StartCoroutine(RandomSteam(steam));
         }
+        StartCoroutine(SparkEngine());
     }
 
     private IEnumerator RandomSteam(GameObject steam)
@@ -33,6 +40,35 @@ public class EngineBody : MonoBehaviour
                 timer += Time.deltaTime;
             }
             steam.SetActive(false);
+        }
+    }
+
+    private IEnumerator SparkEngine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+        while (true)
+        {
+            float timer = Random.Range(0.2f, 1.5f);
+            while(timer >= 0)
+            {
+                yield return null;
+                timer -= Time.deltaTime;
+            }
+            int[] indices = new int[Random.Range(2, 7)];
+            for(int i = 0; i< indices.Length; ++i)
+            {
+                indices[i] = Random.Range(0, sparkSprites.Count);
+            }
+            for(int i = 0; i < indices.Length; ++i)
+            {
+                yield return wait;
+                sparks[nowIdx].sprite = sparkSprites[indices[i]];
+            }
+            sparks[nowIdx++].sprite = null;
+            if(nowIdx >= sparks.Count)
+            {
+                nowIdx = 0;
+            }
         }
     }
 }
